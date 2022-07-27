69da9d9d6331620c997f6353dc9e0cb2265b3cce
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-07-27 06:47:11 +0000

    .

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { Product } = require('../models')
+const { Product } = require('../models');
+const { createProductForm, bootstrapField } = require('../forms');
 
 router.get('/', async function (req, res) {
     let products = await Product.collection().fetch(); // bookshelf syntax for "select * from products"
@@ -10,8 +11,31 @@ router.get('/', async function (req, res) {
     });
 })
 
-router.get('/create', function (req, res) {
-    res.send("create product");
+router.get('/create', async function (req, res) {
+    const productForm = createProductForm();
+    res.render('products/create', {
+        'form': productForm.toHTML(bootstrapField)
+    });
 })
 
-module.exports = router
+router.post('/create', function (req, res) {
+    const productForm = createProductForm();
+    productForm.handle(req, {
+        success: async function (form) {
+            const product = new Product();
+            product.set('name', form.data.name);
+            product.set('cost', form.data.cost);
+            product.set('description', form.data.description);
+            await product.save();
+            res.send(form.data);
+        },
+        error: function (form) {
+            res.render('products/create', {
+                form: form.toHTML(bootstrapField)
+            });
+        },
+        empty: function (form) { }
+    })
+})
+
+module.exports = router;
